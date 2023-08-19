8fbb52159285816e6e71d0f75c67f75763dec40d
Juan Diego Zuniga Arias <juandiegoster@gmail.com>
2023-08-18 21:53:49 -0600

    responsive

--- src/components/home/Formulario.js
+++ src/components/home/Formulario.js
@@ -227,7 +227,7 @@ const Formulario = () => {
 
 
 
-                <Button className="bg-black text-white font-bold py-2 px-4 rounded" type="submit">
+                <Button className="bg-black hover:bg-black-700 text-white font-bold py-2 px-4 rounded" type="submit">
                   Enviar
                 </Button>
             </form>
--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -4,22 +4,28 @@ function Footer() {
     return(
         <div className='bg_page'>
           <div className=" footer-botton-logo flex justify-center">
-            <footer className="footer goBeyond_footer p-10 max-w-5xl  text-base-content">
+            <footer className="footer goBeyond_footer p-10 max-w-5xl  pb-12 sm:pb-24 text-base-content">
               <div className="grid my-10 md:grid-cols-2">
-                <div className='my-10 py-24'>
-                  <Button className="footer-title bg-black text-white font-bold py-2 px-4 rounded">
+                <div className="grid grid-cols-1 gap-4 img-left">
+                  <Button className="footer-title bg-black hover:bg-black-700 text-white font-bold py-2 px-4 rounded">
                     Enlaces de pago
                   </Button>
                   <p>Costo de la tarjeta $49.99</p>
-
                   <p>0x8Dc782d85a6040870B9CCa1D09b13a9bbE0563B4</p>
-                  <img src="/images/qr_usdt.svg" alt="QR USDT" />
-                  <br/>
-                   <p>1JRXH6NfF1sjDqXJRG3XMKw18yGngvseZa</p>
-                  <img src="/images/qr_bitcoin.svg" alt="QR BITCOIN" />
+                  <img
+                    className="max-w-full h-auto mt-2 w-16 md:w-32 lg:w-48"
+                    src="/images/qr_usdt.svg"
+                    alt="QR USDT"
+                  />
+                  <p>1JRXH6NfF1sjDqXJRG3XMKw18yGngvseZa</p>
+                  <img
+                    className="max-w-full h-auto mt-2 w-16 md:w-32 lg:w-48"
+                    src="/images/qr_bitcoin.svg"
+                    alt="QR BITCOIN"
+                  />
                 </div>
-                <div>
-                   <input
+                <div className="img-right">
+                  <input
                     label="Foto comprobante de pago"
                     type="file"
                     accept="image/*"
@@ -28,6 +34,7 @@ function Footer() {
                   />
                 </div>
               </div>
+
 
             </footer>
           </div>
